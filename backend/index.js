79307a1019c1ba26363187d26d9804311d47bd74
Khizar <solkarkhizar27@gmail.com>
2025-01-07 09:59:11 +0530

    fix cors issue

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ import guestRoutes from './routes/Guestroutes.js'
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://boardify-syx3.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  };
+  
+  app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
