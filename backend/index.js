c584b583d9833dbbf0269ff819924f180a0339f4
Khizar <solkarkhizar27@gmail.com>
2025-01-07 10:09:12 +0530

    corse issue update

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,24 @@
 import express from 'express';
 import cors from 'cors'
-import  './db.js';
+import './db.js';
 import hotelroutes from './routes/hotelRoutes.js'
 import guestRoutes from './routes/Guestroutes.js'
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://boardify-syx3.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-  };
-  
-  app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json('Server is running' );
+    res.json('Server is running');
 });
 
 app.use("/uploads", express.static("uploads"));
 
-app.use('/hotels',hotelroutes)
-app.use('/guest',guestRoutes)
+app.use('/hotels', hotelroutes)
+app.use('/guest', guestRoutes)
 
-const PORT = 3000; 
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
